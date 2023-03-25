38eadbadc9fa41a614de341cc1eab087f3a66b73
yijonghoon <yi_jonghoon@naver.com>
2023-03-25 17:25:38 +0900

    20230325

---
+++ 20230325/20230325/20230325.cpp
@@ -0,0 +1,203 @@
+﻿// 20230325.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    int num[2][3] = { {2,2,2}, {1,23,3} };
+
+    std::string alphabet[2][4] = { {"a","b","c","d"},{"e","f","g","h"} };
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cout << num[i][j];
+            if (j != 2) {
+                std::cout << " ";
+            }
+        }
+        if (i != 1) {
+            std::cout << "\n";
+        }
+    }
+    std::cout << "\n";
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << alphabet[i][j];
+            if (j != 3) {
+                std::cout << " ";
+            }
+        }
+        if (i != 1) {
+            std::cout << "\n";
+        }
+    }
+
+    std::cout << std::endl;
+    int num2[3] = { 1, 2, 3 };
+    for (int i = 0; i < 3; i++) {
+        std::cout << num2[i] << " ";
+
+    }
+    std::cout << std::endl;
+
+
+    //for each 문
+    for (int n : num2) {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+    for (auto n : num2) {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+    //// &를 붙여서 참조하고있다는걸 나타냄
+    //for (auto &n : num2) {
+    //    std::cin >> n;
+    //}
+    //std::cout << std::endl;
+
+
+    for (auto &arr : alphabet) {
+        for (auto a : arr) {
+            std::cout << a << " ";
+
+        }
+
+        std::cout << std::endl;
+    }
+
+    std::string city[] = { "Korea","Japan","China","France","Swiss" };
+    for (auto country : city) {
+        std::cout << country << " ";
+    }
+    std::cout << "\n";
+    //std::string city2[5];
+    //for (int i = 0; i < sizeof(city2) / sizeof(city2[0]); i++) {
+    //    std::cin >> city2[i];
+    //}
+
+    //for (auto country : city2) {
+    //    std::cout << country << " ";
+    //}
+    //float score[5];
+    //float length = sizeof(score) / sizeof(score[0]);
+    //float sum = 0;
+    //for (int i = 0; i < length; i++) {
+
+    //    std::cout << i + 1 << "번 학생의 성적을 입력하세요 : ";
+    //    std::cin >> score[i];
+    //}
+    //for (auto grade : score) {
+    //    sum = sum + grade;
+    //}
+    //std::cout << sum / length;
+
+
+
+    //float score[3][2];
+    //float length = sizeof(score) / sizeof(score[0]);
+    //float mathsum = 0;
+    //float koreansum = 0;
+    //for (int i = 0; i < length; i++) {
+    //    for (int j = 0; j < 2; j++) {
+    //        if (j == 0) {
+    //            std::cout << i + 1 << "번 학생의 국어성적을 입력하세요 : ";
+    //            std::cin >> score[i][j];
+    //        }
+    //        else {
+    //            std::cout << i + 1 << "번 학생의 수학성적을 입력하세요 : ";
+    //            std::cin >> score[i][j];
+    //        }
+    //      
+    //    }
+    //    
+    //    
+    //}
+    //for (auto &student : score) {
+    //    int count = 0;
+    //    for (auto grade : student) {
+    //        if (count == 0) {
+    //            koreansum = koreansum + grade;
+    //        }
+    //        else {
+    //            mathsum = mathsum + grade;
+    //        }
+    //        count++;
+    //    }
+    //    count = 0;
+    //}
+    //std::cout << "세 학생의 국어 평균은 : " << koreansum / length << "\n" << "세 학생의 수학 평균은 : " << mathsum / length;
+    // 
+    
+
+    int correct[3] = { 1, 8, 5 };
+    int user[3];
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        std::cout << i+1 << " / 3 " << "중복 없이 1~9 사이의 숫자 3개를 입력하세요 : ";
+        std::cin >> user[i];
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (user[i] == correct[j]) {
+                sum = sum + 1;
+            }
+        }
+    }
+    std::cout << sum << " 개 맞춤!";
+
+
+    //int num1; //변수의 선언
+    //num1 = 0; //변수의 초기화, 할당
+
+    //
+    //int num = 0;
+    //std::string str = "";
+    //// 선언할 때 크기가 정해져야 함.
+    //std::string fruit[3] = { "apple", "banana", "grape"};
+
+    //// 선언하면서 초기화도 동시에 하면 자동으로 배열의 크기가 정해짐
+    //std::string fruit2[] = { "apple", "banana", "grape" };
+
+    //std::string fruit1[3];
+    //fruit1[0] = "aapple";
+    //fruit1[1] = "bbanana";
+    //fruit1[2] = "ggrape";
+
+    ////sizeof() : 변수의 크기를 byte 단위로 변환
+    ////int : 4byte, string : ex. 8byte
+    //// int num[3]; > 12byte, sizeof(num) => 12
+    //for (int i = 0; i < sizeof(fruit)/sizeof(std::string); i++) {
+    //    std::cout << fruit[i] << std::endl;
+    //}
+    //for (int i = 0; i < sizeof(fruit) / sizeof(fruit[0]); i++) {
+    //    std::cout << fruit[i] << std::endl;
+    //}
+    //std::string fruit3[3];
+    //for (int i = 0; i < 3; i++) {
+    //    std::cin >> fruit3[i];
+    //}
+    //for (int i = 0; i < 3; i++) {
+    //    std::cout << fruit3[i] << std::endl;
+    //}
+    ////std::cout << fruit1 << std::endl;
+    ////std::cout << fruit1[0] << std::endl;
+    ////std::cout << fruit1[1] << std::endl;
+    ////std::cout << fruit1[2] << std::endl;
+
+
+
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
---
+++ 20230325afternoon/20230325afternoon/20230325afternoon.cpp
@@ -0,0 +1,154 @@
+﻿// 20230325afternoon.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    int x, y;
+    while (true) {
+        
+        std::cin >> x;
+        std::cin >> y;
+
+        if (x <= 0 || y <= 0) {
+            std::cout << "x와 y 모두 양수를 입력해 주세요." << std::endl;
+        }
+        else {
+            break;
+        }
+    }
+
+    int** arr2 = new int* [x];
+    for (int i = 0; i < x; i++) {
+        arr2[i] = new int[y];
+
+    }
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            arr2[i][j] = j + i*y +1;
+        }
+    }
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            std::cout << arr2[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    int z;
+    std::cin >> z;
+
+    int* arr3 = new int[z];
+
+        for (int i = 0; i < z; i++) {
+            int score;
+            std::cin >> score;
+            arr3[i] = score;
+    }
+        float sum = 0;
+        for (int i = 0; i < z; i++) {
+            sum = sum + arr3[i];
+        }
+        std::cout << sum / z;
+
+
+
+    //포인터 : 메모리 주소를 저장하는 변수
+    //포인터를 이용하면, 동적 메모리를 생성할 수 있다.
+    //포인터가 나오기 전에 배운 변수, 배열 -> 정적메모리
+    // 힙 영역 : 동적 메모리가 저장됨. 할당과 해제를 직접 관리해줘야 한다. delete p; 이 명령어를 통해 할당을 해제할 수 있다.
+    // 스택 영역 : 정적 메모리가 저장됨.
+    // 주의. 포인터를 이용하면 동적 메모리를 생성할 수 있다는 거지, 포인터를 이용한 모든 변수가 동적 메모리는 아님!
+    //int* p = new int;
+    //delete p; //p에 걸린 할당을 해제해줘야 필요없이 메모리를 잡고 있는걸 막아줄 수 있다.
+
+    //int num;
+    //std::cin >> num;
+    ////int* arr = new int[num];
+
+    ////for (int i = 0; i < num; i++) {
+    ////    arr[i] = i + 1;
+    ////}
+    ////for (int i = 0; i < num; i++) {
+    ////    std::cout << arr[i] << " ";
+    ////}
+
+    ////delete[] arr; 
+    //int **arr2 = new int* [num];
+
+    //for (int i = 0; i < num; i++) {
+    //    arr2[i] = new int[num];
+    //}
+
+    //for (int i = 0; i < num; i++) {
+    //    for (int j = 0; j < num; j++) {
+    //        std::cout << arr2[i][j] << " ";
+    //    }
+    //    std::cout << std::endl;
+
+    //}
+    //for (int i = 0; i < num; i++) {
+    //    delete[] arr2[i];
+    //}
+    //delete[] arr2;
+    
+
+    // num * num 행렬
+    // int* arr1 = new int[num]; //일차원 동적 배열
+
+
+   // int n = 3;
+   // int* p = &n;
+
+   // std::cout << p << "\n"; //그냥 변수이름으로 접근하면, 주소값이 담긴다
+   // std::cout << *p <<"\n"; // p에 담긴 주소가 가리키고 있는 값
+   // std::cout << "\n";
+   // std::cout << "\n";
+
+   // n = 10;
+   // std::cout << p << std::endl; //그냥 변수이름으로 접근하면, 주소값이 담긴다
+   // std::cout << *p << std::endl; // p에 담긴 주소가 가리키고 있는 값
+   // std::cout << "\n";
+   // std::cout << "\n";
+   // int n2 = 30;
+   // p = &n2;
+   // std::cout << p << std::endl; //그냥 변수이름으로 접근하면, 주소값이 담긴다
+   // std::cout << *p << std::endl; // p에 담긴 주소가 가리키고 있는 값
+   // std::cout << "\n";
+   // std::cout << "\n";
+   // std::string str = "aaa";
+   // std::string* str_p = &str;
+
+   // std::cout << "str_p는 다음과 같이 나옵니다. \n";
+   // std::cout << str_p << std::endl;
+   // std::cout << "*str_p는 다음과 같이 나옵니다. \n";
+   // std::cout << *str_p << std::endl;
+
+   // std::string& str_pp = *str_p;
+   // std::cout << "str_pp는 다음과 같이 나옵니다. \n";
+   // std::cout << str_pp << std::endl;
+   // std::cout << "&str_pp는 다음과 같이 나옵니다. \n";
+   // std::cout << &str_pp << std::endl;
+
+   // int& num = n;
+   // std::cout << n << std::endl;
+   // std::cout << &n << std::endl;
+   // std::cout << num << std::endl;
+   ///* std::cout << *num << std::endl;*/
+   // std::cout << &num << std::endl;
+
+
+
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
