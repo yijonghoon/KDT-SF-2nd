d8f962a4f6284c536d7765f39bc213cf4bfa7a3f
yijonghoon <yi_jonghoon@naver.com>
2023-03-26 15:43:11 +0900

    20230326

---
+++ 20230326과제/20230326과제/20230326과제.cpp
@@ -0,0 +1,101 @@
+﻿// 20230326과제.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+
+int main()
+{
+
+    std::vector<char> str_set;
+
+    std::string str;
+    std::getline(std::cin, str);
+
+    for (int i = 0; i < str.length(); i++) {
+        bool is_in = false;
+        if (str[i] != ' '){
+            for (int j = 0; j < str_set.size(); j++) {
+   
+                if (str[i] == str_set.at(j)) {
+                    is_in = true;
+                }
+            
+        }
+        if (is_in == false) {
+            str_set.push_back(str[i]);
+        }}
+        
+    }
+    std::cout << "\n" << str_set.size() << " 개 (" ;
+
+    for (int i = 0; i < str_set.size()-1; i++) {
+        std::cout << str_set[i] << ", ";
+    }
+    std::cout << str_set[str_set.size()-1] << " )";
+
+
+
+    int x, y;
+    while (true) {
+
+        std::cin >> x;
+        std::cin >> y;
+
+        if (x <= 0 || y <= 0) {
+            std::cout << "x와 y 모두 양수를 입력해 주세요." << std::endl;
+        }
+        else {
+            break;
+        }
+    }
+    std::vector<std::vector<int>> arr = { };
+    for (int i = 0; i < x; i++) {
+        arr.push_back({});
+        for (int j = 0; j < y; j++) {
+            arr[i].push_back(j + i * y + 1);
+        }
+    }
+
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::vector<int> makesum= {};
+
+    while(true){
+        std::cout << std::endl << "숫자를 입력하세요 (0 : exit)  : ";
+        int num;
+        std::cin >> num;
+        makesum.push_back(num);
+
+        if (num == 0) {
+            break;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < makesum.size(); i++) {
+        sum = sum + makesum[i];
+    }
+
+    std::cout << "사용자가 입력한 수의 합은 " << sum << " 입니다. \n";
+
+    
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
---
+++ 20230326오전/20230326오전/20230326오전.cpp
@@ -0,0 +1,72 @@
+﻿
+
+#include <iostream>
+#include <vector>
+
+namespace test {
+    int num = 1;
+    int sum(int n, int n2) {
+        return n + n2;
+    }
+}
+
+int main()
+{
+    int num = 0;
+    std::cout << num << std::endl;
+    std::cout << test::num << std::endl;
+
+    std::string str = "aa";
+
+
+    std::vector<int> v0(4);
+    // 크기가 4인 배열을 만듦. 모든 값 0으로 초기화
+
+    std::vector<int> v1 = { 1, 2, 3 };
+    //크기가 3인 배열. 원소가 1, 2, 3 으로 초기화
+  
+    std::vector<int> v22(4, 1);
+    //크기가 4인 배열을 만듦. 모든 값을 1로 초기화
+    //{1,1,1,1}
+    std::vector<int> v3;
+    v3.assign(5, 2);
+    // 크기가 5인 배열을 만듦. 모든 값을 2로 초기화
+
+    std::vector<int> v(7);
+    v.assign(5, 2);
+    // 벡터의 크기가 5인 배열로 만듦 모두 2로 초기화
+    // v.size()로 벡터의 사이즈를 가져올 수 있음
+    // v.at()로 
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " "; // out of range 오류가 발생해도 잘 잡아주지 않는다.
+        std::cout << v.at(i) << std::endl; // 오류를 잡아줌
+    }
+    std::cout << "------------------------------------" << std::endl;
+    v.push_back(6); // vector의 제일 마지막에 원소를 push함 {2,2,2,2,2,6}
+    v.pop_back(); // 제일 마지막 원소를 꺼낸다 {2,2,2,2,2,}
+    v.push_back(7); // {2,2,2,2,2,7}
+    v.begin(); // vector의 시작 주소를 반환함.
+    v.insert(v.begin(), 5); //{5,2,2,2,2,2,6}
+    v.erase(v.begin(),v.begin()+3); // {2,2,2,6}
+
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " "; // index오류가 발생해도 프로그램이 잡지 못하기때문에 아래에 있는 v.at(i)를 쓰는것이다. 확시랗게 하기 위해서. 근데 버그 재현이 안되고있는 상황
+        std::cout << v.at(i) << std::endl;
+    }
+    v.clear(); // { }
+
+    //이차원배열
+    std::vector<std::vector<int>> v2 = {{1,2},{3,4}};
+    for (int i = 0; i < v2.size(); i++) {
+        for (int j = 0; j < v2.at(i).size(); j++) {
+            std::cout << v2.at(i).at(j) << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+
+
+    return 0;
+}
+
